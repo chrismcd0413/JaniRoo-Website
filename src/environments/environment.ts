d7fe3ec1ad04b2fe7267e2dd590b4c8fbb096a23
chrismcd0413 <40510393+chrismcd0413@users.noreply.github.com>
2024-01-04 10:57:09 -0600

    Update environment.ts
    Removed API Credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAu3qu3yOWb1i0_vKBpRm_uFplCaXF3U-g',
-    authDomain: 'janiroo-2dcab.firebaseapp.com',
-    projectId: 'janiroo-2dcab',
-    storageBucket: 'janiroo-2dcab.appspot.com',
-    messagingSenderId: '94772906514',
-    appId: '1:94772906514:web:e07138fba67e7753df66cb',
-    measurementId: 'G-Z1N22YJ8QT',
+    /* Hidden */
   }
 };
 
